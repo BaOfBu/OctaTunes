11266dace4dddc68d7cd0db62b9263fcd4c0fc46
minhchau1112 <tnmchau1211@gmail.com>
2024-04-26 22:32:08 +0700

    fix bug

--- app/src/main/java/com/example/octatunes/Activity/ListenToMusicActivity.java
+++ app/src/main/java/com/example/octatunes/Activity/ListenToMusicActivity.java
@@ -236,7 +236,7 @@ public class ListenToMusicActivity extends Fragment implements View.OnClickListe
                     MusicService.mediaPlayer.seekTo(progress);
                     if(mLyricView != null){
                         mLyricView.setCurrentTimeMillis(progress);
-                        handlerLyric.postDelayed(updateProgress, 500);
+//                        handlerLyric.postDelayed(updateProgress, 500);
                     }
                 }
             }
@@ -430,7 +430,7 @@ public class ListenToMusicActivity extends Fragment implements View.OnClickListe
                     }else{
                         play.setImageResource(R.drawable.ic_circle_play_white_70);
                     }
-                    handlerLyric.postDelayed(updateProgress, 500);
+//                    handlerLyric.postDelayed(updateProgress, 500);
                     playTime.setText(MusicUtils.formatTime(MusicService.mediaPlayer.getCurrentPosition()));
                     seekBar.setProgress(MusicService.mediaPlayer.getCurrentPosition());
                     //mLyricView.setCurrentTimeMillis(MusicService.mediaPlayer.getCurrentPosition());
@@ -576,7 +576,7 @@ public class ListenToMusicActivity extends Fragment implements View.OnClickListe
                 @Override
                 public void onClick(View v) {
                     bottomSheetDialog.cancel();
-                    int delay = (currentSong.getDuration() + 2) * 1000 - 300 - MusicService.mediaPlayer.getCurrentPosition();
+                    int delay = (currentSong.getDuration() + 2) * 1000 - MusicService.mediaPlayer.getCurrentPosition();
                     handleScheduleAlarm(delay);
                     if(isOnAlarm){
                         title.setText("Hẹn giờ đi ngủ - Cuối bản nhạc");
