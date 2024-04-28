42f5bce0db9449cd6cb8cd9c73539d8aff19cacc
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2024-04-28 14:42:06 +0700

    temp commit

--- app/src/main/java/com/example/octatunes/Adapter/SongAdapter.java
+++ app/src/main/java/com/example/octatunes/Adapter/SongAdapter.java
@@ -1,6 +1,12 @@
 package com.example.octatunes.Adapter;
 
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,10 +33,20 @@ import com.example.octatunes.Model.PlaylistsModel;
 import com.example.octatunes.Model.TracksModel;
 import com.example.octatunes.R;
 import com.example.octatunes.Services.TrackService;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
 
@@ -38,6 +54,7 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
     private List<TracksModel> songList;
     private FragmentListener listener;
     private PlaylistsModel playList;
+    Handler checkDownload;
 
     public SongAdapter(Context context, List<TracksModel> songList, FragmentListener listener) {
         this.context = context;
@@ -110,9 +127,106 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
             titleBottomSheet.setText(track.getName());
             item_artist.setText(holder.itemArtist.getText());
 
+            TextView share = bottomSheetDialog.findViewById(R.id.share);
+            share.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        downloadFile(track.getFile(), track.getName(), context);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+
             bottomSheetDialog.show();
         });
     }
+
+    public void downloadFile(String url,String filename, Context context) throws InterruptedException {
+        String DownloadUrl = url;
+        DownloadManager.Request request1 = new DownloadManager.Request(Uri.parse(DownloadUrl));
+        request1.setDescription("Download Music for Share");   //appears the same in Notification bar while downloading
+        request1.setTitle(filename+".mp3");
+        request1.setVisibleInDownloadsUi(true);
+
+        request1.allowScanningByMediaScanner();
+        request1.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN);
+        request1.setDestinationInExternalFilesDir(context, "/MusicFolder" ,filename + ".mp3");
+
+        DownloadManager manager1 = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        long downloadId = Objects.requireNonNull(manager1).enqueue(request1);
+        Cursor cursor = manager1.query(new DownloadManager.Query().setFilterById(downloadId));
+        checkDownload = new Handler();
+        checkDownload.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Boolean flag = checkDownloadStatus(cursor, filename);
+                if (flag) {
+                    checkDownload.removeCallbacksAndMessages(null);
+                }
+                else{
+                    checkDownload.postDelayed(this, 500);
+                }
+            }
+        }, 500);
+    }
+    private Boolean checkDownloadStatus(Cursor cursor, String filename) {
+        if (cursor.moveToFirst()) {
+            File[] externalFilesDirs = context.getExternalFilesDirs(null);
+            if (externalFilesDirs != null && externalFilesDirs.length > 0) {
+                // Get the first external files directory (primary storage)
+                File primaryExternalDir = externalFilesDirs[0];
+
+                String fileName = filename + ".mp3";
+
+                // Construct the full path to the file within the "LyricFolder" subfolder
+                File subFolder = new File(primaryExternalDir, "MusicFolder");
+                File file = new File(subFolder, fileName);
+                Log.d("Download", "Music downloaded to: " + file.getAbsolutePath());
+                Log.d("Download", "Music downloaded existed ?: " + file.exists());
+                if (file.exists()) {
+                    copyFileToExternalStorageAndShare(file);
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void copyFileToExternalStorageAndShare(File mp3File) {
+        File externalStorageDir = Environment.getExternalStorageDirectory(); // Get external storage directory
+        File newFile = new File(externalStorageDir, mp3File.getName()); // Create a new file in external storage directory
+        Log.d("Download", "New file path: " + newFile.getAbsolutePath());
+        try {
+            Log.d("Download", "In Try block...");
+            FileInputStream inStream = new FileInputStream(mp3File);
+            FileOutputStream outStream = new FileOutputStream(newFile);
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = inStream.read(buffer)) > 0) {
+                Log.d("Download", "Copying file...");
+                outStream.write(buffer, 0, length);
+            }
+            inStream.close();
+            outStream.close();
+            // Share the copied file
+            Intent share = new Intent(Intent.ACTION_SEND);
+            share.setType("audio/mp3");
+            share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(newFile));
+            share.putExtra(Intent.EXTRA_SUBJECT, "Sharing File...");
+            share.setPackage("com.google.android.gm");
+            context.startActivity(Intent.createChooser(share, "Share Sound File"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 
     private void loadImageForTrack(TracksModel track, ImageView imageView) {
         TrackService trackService = new TrackService();
