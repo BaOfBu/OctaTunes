2d6c9ab7f25f99b8d618724fecafb960d62a4f41
minhchau1112 <tnmchau1211@gmail.com>
2024-05-04 12:25:27 +0700

    change media2 into media3

--- app/src/main/java/com/example/octatunes/Activity/DeviceSongFragment.java
+++ app/src/main/java/com/example/octatunes/Activity/DeviceSongFragment.java
@@ -112,7 +112,7 @@ public class DeviceSongFragment extends Fragment {
                 String title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
                 String artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST));
                 int duration = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)) / 1000;
-                String image = "https://firebasestorage.googleapis.com/v0/b/octatunes-495d2.appspot.com/o/images%2Falbums%2F001.jpg?alt=media&token=d802b4c3-b59e-4c76-85a5-2cd34f9892b5";
+                String image = "https://firebasestorage.googleapis.com/v0/b/octatunes-495d2.appspot.com/o/images%2Falbums%2F004.jpg?alt=media&token=5761d139-98a0-409c-92a2-5ea55e843155";
 
                 // Handle unknown artist
                 if (artist == null || artist.isEmpty()) {
